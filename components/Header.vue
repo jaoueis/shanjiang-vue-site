60f0fa0fb12bea4525a7461b0eccf33aedf22f47
Shan Jiang <shan.jiang@ghd.com>
2022-11-01 19:17:26 -0700

    Handle header and mobile header

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,11 @@
 				<Logo></Logo>
 			</div>
 			<div>
+				<button class="mobile-menu-toggle" :aria-expanded="isActive" aria-label="Toggle the main menu" @click="toggleMenu">
+					<span></span>
+				</button>
+			</div>
+			<div :class="isActive === 'false' ? '' : 'toggled'">
 				<nav>
 					<ul>
 						<li>
@@ -28,7 +33,22 @@ export default {
 			navs: {
 				about: 'About',
 				contact: 'Contact',
-			}
+			},
+			isActive: 'false'
+		}
+	},
+	mounted() {
+		window.addEventListener('resize', this.onResize);
+	},
+	beforeDestroy() {
+		window.removeEventListener('resize', this.onResize);
+	},
+	methods: {
+		toggleMenu() {
+			this.isActive = this.isActive === 'false' ? 'true' : 'false';
+		},
+		onResize() {
+			this.isActive = 'false'
 		}
 	}
 }
@@ -36,6 +56,8 @@ export default {
 
 <style lang="scss">
 header {
+	z-index: 9 !important;
+
 	.header-inner {
 		display: flex;
 		align-items: center;
@@ -44,20 +66,131 @@ header {
 		width: 100%;
 		max-width: 1400px;
 		margin: 0 auto;
+
+		>div {
+			&:last-child {
+				position: fixed;
+				top: 0;
+				left: 100%;
+				width: 100%;
+				height: 100%;
+				visibility: hidden;
+				transition: all 0.4s cubic-bezier(0, 0, .58, 1);
+
+				@media (min-width: 992px) {
+					position: static;
+					width: auto;
+					background-color: transparent;
+					visibility: visible;
+				}
+
+				&.toggled {
+					top: 0;
+					left: 0;
+					background-color: var(--brand-dark);
+					visibility: visible;
+				}
+			}
+		}
+	}
+
+	.mobile-menu-toggle {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		width: 30px;
+		height: 20px;
+		background-color: transparent;
+		border: none;
+
+		@media (min-width: 992px) {
+			display: none;
+		}
+
+		&[aria-expanded='true'] {
+			z-index: 1;
+
+			span {
+				background-color: transparent;
+
+				&::before,
+				&::after {
+					width: 26px;
+					transform-origin: 1px 50%;
+					background-color: var(--brand-light);
+				}
+
+				&::before {
+					transform: rotate(45deg);
+				}
+
+				&::after {
+					transform-origin: 0 50%;
+					transform: rotate(-45deg);
+				}
+			}
+		}
+
+		span {
+			display: block;
+			width: 20px;
+			height: 2px;
+			background-color: var(--brand-dark);
+			transition: all 0.3s cubic-bezier(0, 0, .58, 1);
+
+			&::before,
+			&::after {
+				position: absolute;
+				left: 0;
+				display: block;
+				width: 30px;
+				height: 2px;
+				background-color: var(--brand-dark);
+				content: '';
+				transition: all 0.3s cubic-bezier(0, 0, .58, 1);
+			}
+
+			&::before {
+				top: 0;
+			}
+
+			&::after {
+				bottom: 0;
+			}
+		}
 	}
 
 	nav {
+		width: 100%;
+		height: 100%;
+
 		ul {
 			display: flex;
-			list-style: none;
 			gap: 2rem;
+			list-style: none;
+
+			@media (max-width: 991px) {
+				flex-direction: column;
+				justify-content: center;
+				width: 100%;
+				height: 100%;
+				padding: 3rem;
+			}
 
 			li {
 				a {
-					font-size: 1rem;
+
+					color: #fff;
+					font-size: 2rem;
 					font-weight: 600;
 					text-decoration: none;
 					text-transform: lowercase;
+
+					@media (min-width: 992px) {
+						color: var(--brand-dark);
+						font-size: 1rem;
+					}
 
 					&:hover,
 					&:focus {

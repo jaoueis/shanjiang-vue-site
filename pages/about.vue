124a6f6c928f97040bf4fad98c7296827faa4944
Shan Jiang <shan.jiang@ghd.com>
2022-11-01 20:44:48 -0700

    Update bg method

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,7 @@
 <template>
-	<div class="page-canvas" ref='vantaRef'>
+	<div class="page-canvas">
 		<Header></Header>
 		<About></About>
 		<Footer></Footer>
 	</div>
 </template>
-
-<script>
-import TOPOLOGY from 'vanta/dist/vanta.topology.min'
-
-export default {
-	name: 'AboutPage',
-	mounted() {
-		this.vantaEffect = TOPOLOGY({
-			el: this.$refs.vantaRef,
-			color: 0xddc086,
-			backgroundColor: 0xffffff
-		})
-	},
-	beforeDestroy() {
-		if (this.vantaEffect) {
-			this.vantaEffect.destroy()
-		}
-	}
-}
-</script>
--- plugins/calcHeight.js
+++ plugins/calcHeight.js
@@ -1,7 +1,19 @@
 const appHeight = () => {
 	const doc = document.documentElement;
-
 	doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+
+	VANTA.TOPOLOGY({
+		el: ".page-canvas",
+		mouseControls: true,
+		touchControls: true,
+		gyroControls: false,
+		minHeight: 200.00,
+		minWidth: 200.00,
+		scale: 1.00,
+		scaleMobile: 1.00,
+		color: 0xddc086,
+		backgroundColor: 0xffffff
+	})
 }
 
 window.addEventListener('load', appHeight);
